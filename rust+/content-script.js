d005a407197799a36eb6103665774952948c5f10
Сергей Негашев <92201245+IdevuUkom@users.noreply.github.com>
2023-03-11 11:23:01 +0500

    Add files via upload

---
+++ rust+/content-script.js
@@ -0,0 +1,13 @@
+function deleteBlock() {
+    console.log (456)
+    !!document
+    .getElementsByClassName(
+        "currency_rates__container-3P"
+        ) &&
+        document
+    .getElementsByClassName(
+        "currency_rates__container-3P"
+        )[0].remove();
+}
+
+deleteBlock();
